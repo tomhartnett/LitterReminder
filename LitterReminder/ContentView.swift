26f60b37435905c75d4b8398d21d6e71b80ddeaa
Tom Hartnett <tomhartnett@gmail.com>
2024-11-17 09:29:31 -0600

    Initial Commit

---
+++ LitterReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LitterReminder
+//
+//  Created by Tom Hartnett on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LitterReminder/LitterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitterReminderApp.swift
+//  LitterReminder
+//
+//  Created by Tom Hartnett on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LitterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
